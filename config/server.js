b68d2a964d076f82fc54260d422d7f185ff02025
viniciuskmv <viniciuskmv@gmail.com>
2021-09-07 09:41:11 -0300

    Commit inicial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3c9bd781684528693e9a9d4906d9196f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '84b23696-f51f-41cb-a3db-2015758f316c'
+};
